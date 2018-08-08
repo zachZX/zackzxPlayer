83d8ae2b21b537cf247e8459530b81ee079b710a
zhangxiong <137062407@qq.com>
2018-08-08 14:54:48 +0800

    Initial Commit

---
+++ 播放器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  播放器
+//
+//  Created by YuanCan on 2018/8/8.
+//  Copyright © 2018年 zhangxion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 播放器/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  播放器
+//
+//  Created by YuanCan on 2018/8/8.
+//  Copyright © 2018年 zhangxion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
